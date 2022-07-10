ffa6a5f27703cc591ff09ceae962249ba3f16653
illya bondar <1816579@gmail.com>
2022-07-10 20:40:05 +0300

    close modal on keydown added

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,13 +1,31 @@
+import React from 'react';
 import css from './Modal.module.css';
 
-const Modal = ({ overview, closeModal }) => {
-  return (
-    <div className={css.backdrop} onClick={closeModal}>
-      <div className={css.modal}>
-        <p>{overview}</p>
+class Modal extends React.Component {
+  componentDidMount() {
+    window.addEventListener('keydown', this.onEscape);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.onEscape);
+  }
+
+  onEscape = e => {
+    if (e.code === 'Escape') {
+      this.props.closeModal();
+    }
+  };
+
+  render() {
+    const { overview, closeModal } = this.props;
+    return (
+      <div className={css.backdrop} onClick={closeModal}>
+        <div className={css.modal}>
+          <p>{overview}</p>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Modal;
